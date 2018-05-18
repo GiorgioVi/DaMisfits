ed416148e78a86961858adc478a030920870d105
Jacqueline Xu <jackiestuy@gmail.com>
2018-05-18 14:35:06 -0400

    reformat log.sh

--- main.py
+++ main.py
@@ -85,53 +85,6 @@ def profile():
         db.remove_favorite(session[USER_SESSION], request.form["songID"])#must be a post request so remove the desired song
         return redirect(url_for("profile"))
 
-@app.route("/song", methods = ["GET", "POST"])
-def song():
-    if request.method == "GET":
-        title = request.args.get("title")
-        artist = request.args.get("artist")
-        id = api.get_song_id(title, artist)
-        if id == 0:#if the song was not found
-            return render_template("error.html", error = "Song not found", title = title, artist = artist, isLogged = (USER_SESSION in session))
-        lyrics = api.get_lyrics(id)
-        if lyrics == 0:
-            return render_template("error.html", error = "Lyrics not found", title = title, artist = artist, isLogged = (USER_SESSION in session))
-        api.get_wav(lyrics, "./static/{}.wav".format(title))
-        return render_template("song.html", title = title, artist = artist, lyrics = lyrics, id = id, isLogged = (USER_SESSION in session), url = url_for("static", filename = "{}.wav".format(title)))#return page
-    if "favorite" in request.form:#if they want to add to favorites
-        print request.form["favorite"]
-        title = request.form["title"]
-        artist = request.form["artist"]
-        id = request.form["id"]
-        if USER_SESSION in session:#check if user in session
-            if db.is_favorite(session[USER_SESSION], request.form["favorite"]):
-                flash("Song is already in your favorites list.")
-            else:
-                db.add_favorite(session[USER_SESSION], request.form["favorite"])#add the song to their fav
-                flash("Song has been added to your favorites")
-            lyrics = api.get_lyrics(id)
-            id = api.get_song_id(title, artist)
-            return render_template("song.html", title = title, artist = artist, lyrics = lyrics, id = id, isLogged = (USER_SESSION in session), url = url_for("static", filename = "{}.wav".format(title)))#return page#re render the page
-        else:
-            flash("Please login to add to your favorites.")
-            return redirect(url_for("login"))#if they arent logged in, then send them to the login page
-
-@app.route("/artist")
-def artist():
-    artist = request.args.get("artist")
-    id = api.get_artistid(artist)
-    if id == 0:#if the song was not found
-        return render_template("error.html", error = "Artist not found", title = "", artist = artist, isLogged = (USER_SESSION in session))
-    albums = api.get_albums(id)
-    album_dict = {}
-    for i in albums:
-        album_dict[i["album"]["album_name"]] = api.get_album_tracks(i["album"]["album_id"])
-    return render_template("artist.html", artist = artist, album_dict = album_dict, isLogged = (USER_SESSION in session))
-
-@app.route("/search")
-def search():
-    return render_template("search.html", isLogged = (USER_SESSION in session))
-
 if __name__ == "__main__":
     d = sqlite3.connect("data/database.db")
     c = d.cursor()
